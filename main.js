d56add5af1c988f2ae9b18e123926348cc4e198a
Guilherme Abreu <gglabreu@hotmail.com>
2022-04-25 22:44:15 -0300

    feat: social icon animations

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 /* change the header when scroll the page */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     // scroll bigger than the header height
     header.classList.add('scroll')
@@ -29,7 +29,7 @@ window.addEventListener('scroll', function () {
     // smaller than header height
     header.classList.remove('scroll')
   }
-})
+}
 
 /* swiper animation */
 const swiper = new Swiper('.swiper-container', {
@@ -61,18 +61,24 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials, 
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
 
-// show back to top button
+// when scroll
 
-const backToTopButton = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= 600) {
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 570) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
 })
